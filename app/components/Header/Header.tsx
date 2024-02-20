d118078b7083aa670cc3b7d5327476d30572383e
Joe simpson <joe@egmrc.com>
2024-02-20 13:49:12 -0700

    Fix content being wider than screen

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -10,13 +10,13 @@ import Link from "next/link";
 
 const navArr = [
     {
-        item:{
+        item: {
             title: "Register",
             url: "/register"
         }
     },
     {
-        item:{
+        item: {
             title: "Support",
             url: "/support",
         },
@@ -50,10 +50,11 @@ const Header = () => {
 
     return (
         <>
-            <header className="bg-primary-800 flex justify-end items-center w-full lg:h-[125px] relative">
+            <header className="bg-primary-800 flex justify-end items-center w-full lg:h-[125px] relative z-50">
                 <div className="flex flex-col lg:flex-row justify-between w-full items-center max-w-[1800px]">
                     <Link href="/" className="relative lg:pl-44 lg:mr-28 flex overflow-hidden lg:overflow-visible">
-                        <Image className="p-1 lg:absolute w-24 h-24 sm:h-auto sm:w-40 left-0 mr-7 z-20" src={logoImg} alt=""/>
+                        <Image className="p-1 lg:absolute w-24 h-24 sm:h-auto sm:w-40 left-0 mr-7 z-20" src={logoImg}
+                               alt=""/>
                         <h1 className={`${inika.className} text-2xl pt-2.5 xl:text-4xl font-bold pr-10 sm:pr-0`}>Black
                             Hills
                             Defense <br
@@ -64,7 +65,7 @@ const Header = () => {
                         className="hidden bg-primary-600 pl-4 lg:pr-52 md:w-6/12 md:flex justify-between relative mt-3 z-30">
                         {
                             navArr.map(item => {
-                                return(
+                                return (
                                     <NavItem item={item.item} subItems={item.subItems} key={item.item.title}/>
                                 )
                             })
@@ -74,7 +75,7 @@ const Header = () => {
                 <button onClick={() => {
                     setMobileMenuIsOpen(!mobileMenuIsOpen);
                 }}
-                        className="md:hidden font-bold top-2 right-2 absolute size-10 hover:opacity-100 z-50 bg-primary-800">
+                        className="md:hidden font-bold top-2 right-2 fixed size-10 hover:opacity-100 z-50 bg-primary-800">
                     {
                         mobileMenuIsOpen ?
                             <FontAwesomeIcon size="lg" icon={faCircleXmark}/> :
@@ -82,18 +83,18 @@ const Header = () => {
                     }
 
                 </button>
+                <nav className={`h-full top-0 w-full z-40 pt-12 fixed transition-transform ${
+                    mobileMenuIsOpen ? "" : "translate-x-full md:hidden"
+                }`}>
+                    <div className="w-8/12 text-primary-900 p-2 ml-auto h-full bg-zinc-900">
+                        {
+                            navArr.map(item => {
+                                return <NavItem item={item.item} subItems={item.subItems} key={item.item.title} mobile/>
+                            })
+                        }
+                    </div>
+                </nav>
             </header>
-            <nav className={`h-full absolute top-0 w-full z-40 pt-12 transition-transform ${
-                mobileMenuIsOpen ? "" : "translate-x-full md:hidden"
-            }`}>
-                <div className="w-8/12 text-primary-900 p-2 ml-auto h-full bg-zinc-900">
-                    {
-                        navArr.map(item => {
-                            return <NavItem item={item.item} subItems={item.subItems} key={item.item.title} mobile/>
-                        })
-                    }
-                </div>
-            </nav>
         </>
     );
 }
