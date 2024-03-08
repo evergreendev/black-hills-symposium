6203f4a968a3d70ff7004228769b36b76e691fb5
Joe simpson <joe@egmrc.com>
2024-03-08 14:05:41 -0700

    Update agenda

--- app/components/Agenda/Agenda.tsx
+++ app/components/Agenda/Agenda.tsx
@@ -9,25 +9,7 @@ type timeItem = {
 }
 
 
-const schedule: dateItem[] = [{
-    date: "Wednesday, April 10, 2024",
-    items: [
-        {
-            time: "4:30-5:00pm",
-            items: [
-                "Featured Speaker - Main Ballroom"
-            ]
-        },
-        {
-            time: "5:00-7:00pm",
-            items: [
-                "South Dakota Mines Social- Foyer",
-                "Vendor Booths Open"
-            ]
-        }
-    ]
-},
-    {
+const savedCallback =     {
         callout: true,
         date: "In-depth pre-symposium sessions, April 10. Open to all symposium attendees.",
         items: [
@@ -44,82 +26,141 @@ const schedule: dateItem[] = [{
                 ]
             }
         ]
-    },
+    };
+
+const schedule: dateItem[] = [{
+    date: "Wednesday, April 10, 2024",
+    items: [
+        {
+            time: "8:30am",
+            items: [
+                "Opening Ceremonies / Pledge of Allegiance"
+            ]
+        },
+        {
+            time: "8:40am - 11:00am",
+            items: [
+                "PFOS/PFOA Technical Seminar*"
+            ]
+        },
+        {
+            time: "11:00am - 1:00pm",
+            items: [
+                "Lunch on your own"
+            ]
+        },
+        {
+            time: "1:00pm - 4:20pm",
+            items: [
+                "PFOS/PFOA Technical Seminar*"
+            ]
+        },
+        {
+            time: "1:00pm",
+            items: [
+                "War Games* Kickoff"
+            ]
+        },
+        {
+            time: "1:15pm - 4:20pm",
+            items: [
+                "The Perils of Theater Nuclear Escalation War Games, to feature panels of participants actively participating in a war game simulation. Panels will consist of military and civilian panelists. Simulation will demonstrate the complexity of a potential conflict with a peer nation and the challenges the United States would encounter."
+            ]
+        },
+        {
+            time: "4:30pm - 5:00pm",
+            items: [
+                "Featured Speaker - Main Ballroom"
+            ]
+        },
+        {
+            time: "5:00pm - 7:00pm",
+            items: [
+                "South Dakota Mines Social- Foyer",
+                "Vendor Booths Open"
+            ]
+        }
+    ]
+},
     {
         date: "Thursday, April 11, 2024",
         items: [
             {
                 time: "7:00am",
                 items: [
-                    "Networking/Coffee Social"
+                    "Networking/Coffee Social - Foyer"
                 ]
             },
             {
                 time: "8:00am",
                 items: [
-                    "Welcome",
-                    "South Dakota Mines Pep Band/Honor Guard/National Anthem"
+                    "Opening Ceremonies / Pledge of Allegiance",
+                    "To include: South Dakota Mines Pep Band/Honor Guard/National Anthem"
                 ]
             },
             {
                 time: "8:15am",
                 items: [
-                    "Featured Speakers: Congressman Dusty Johnson (via video) & Maj Gen Morrell, SDNG AG"
+                    "Featured Speakers: Congressman Dusty Johnson (via video)",
+                    "Maj Gen Morrell, SDNG AG"
                 ]
             },
             {
-                time: "9:00am",
+                time: "9:00am - 9:50am",
                 items: [
-                    "Track Sessions"
+                    "Track Session A"
                 ]
             },
             {
-                time: "10:00am",
+                time: "10:00am - 10:50am",
                 items: [
-                    "Connection Break- Foyer"
+                    "Connection Break - Foyer"
                 ]
             },
             {
-                time: "11:00am",
+                time: "11:00am - 11:50am",
                 items: [
-                    "Track Sessions"
+                    "Track Session B"
                 ]
             },
             {
                 time: "12:00pm",
                 items: [
-                    "MAC Chow Hall Luncheon - Welcome Video - Tim Houghton, Symposium Chair",
+                    "MAC Chow Hall Luncheon",
                     "Featured Speaker: Lt Gen Caroline Miller, HAF/A1"
                 ]
             },
             {
-                time: "1:30pm",
+                time: "1:30pm - 2:20pm",
                 items: [
-                    "Track Session"
+                    "Track Session C"
                 ]
             },
             {
-                time: "2:30pm",
+                time: "2:30pm - 3:20pm",
                 items: [
-                    "Track Sessions"
+                    "Track Session D"
                 ]
             },
             {
-                time: "3:30pm",
+                time: "3:30pm - 3:55pm",
                 items: [
-                    "B21 Update - Col. Derek Oakley, Commander of the 28th Bomb Wing, EAFB"
+                    "B21 Raider Update - Col. Derek Oakley, Commander of the 28th Bomb Wing, EAFB"
                 ]
             },
             {
-                time: "4:00pm-5:00pm",
+                time: "4:00pm - 5:00pm",
                 items: [
-                    "Elevate Social, Drawings, Grand Prize - Foyer"
+                    "Elevate and HS Precision Social - Foyer",
+                    "Prize Drawings"
                 ]
             },
             {
                 time: "5:30pm",
                 items: [
-                    "Tours depart"
+                    "Tours depart",
+                    "SD Mines Lab Tour",
+                    "Mt. Rushmore & Dinner Tour"
                 ]
             }
         ]
@@ -214,7 +255,8 @@ const Agenda = () => {
             <h2 className="text-4xl font-bold p-2.5">Events</h2>
             {
                 schedule.map((x) => {
-                    return <div key={x.date} className={`mb-10 px-16 ${x.callout ? "bg-primary-200 py-6 shadow-lg" : ""}`}>
+                    return <div key={x.date}
+                                className={`mb-10 px-16 ${x.callout ? "bg-primary-200 py-6 shadow-lg" : ""}`}>
                         <p className="text-2xl font-bold mb-4">{x.date}</p>
                         <div>
                             {x.items.map((y) => {
