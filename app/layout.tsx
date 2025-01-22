54a4ab20c935b4bce3e8d63a36c583f079317ca6
Joe simpson <joe@egmrc.com>
2025-01-22 14:54:24 -0700

    Add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import {inter} from './fonts';
 import Header from "@/app/components/Header";
 import Footer from "@/app/components/Footer";
+import { GoogleAnalytics } from "@next/third-parties/google"
 
 
 export const metadata: Metadata = {
@@ -17,6 +18,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" className="overflow-x-hidden">
+        <GoogleAnalytics gaId={"G-0C5VGMKCLG"}/>
         <body className={`${inter.className} overflow-x-hidden text-white`}>
         <Header/>
         {children}
