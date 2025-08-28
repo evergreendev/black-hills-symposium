e1e87f68d09ce97c23f7d045482763b87967c66f
Joe simpson <joe@egmrc.com>
2025-08-28 11:46:13 -0600

    Remove unused image import from `ImageSlider` component

--- app/components/ImageSlider/ImageSlider.tsx
+++ app/components/ImageSlider/ImageSlider.tsx
@@ -3,7 +3,6 @@ import React, {useEffect, useState} from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import imgA from "@/public/2025/3769158129.jpg";
 import imgB from "@/public/2025/BH Defense Sym.-034.jpg";
 import imgC from "@/public/2025/BH Defense Sym.-082.jpg";
 import imgD from "@/public/2025/BH Defense Sym.-085.jpg";
@@ -13,7 +12,7 @@ import imgG from "@/public/2025/BH Defense Sym.-210.jpg";
 import imgH from "@/public/2025/BH Defense Sym.-274.jpg";
 import Image from "next/image";
 
-const imgArr = [imgA,imgB,imgC,imgD,imgE,imgF,imgG,imgH];
+const imgArr = [imgB,imgC,imgD,imgE,imgF,imgG,imgH];
 
 const ImageSlider = () => {
     const [screenWidth, setScreenWidth] = useState(1000);
