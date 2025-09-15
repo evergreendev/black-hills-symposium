fee4ff714b83eb577bebb300fa70828eff7f0f14
Joe simpson <joe@egmrc.com>
2025-09-15 10:41:58 -0600

    Update hotel information text for 2026 Symposium, refine styles, and revise booking details.

--- app/hotel-information/page.tsx
+++ app/hotel-information/page.tsx
@@ -18,20 +18,26 @@ export default function About() {
                     <div className="w-full max-w-prose text-primary-900 text-xl space-y-3.5">
                         <h2 className="font-bold text-5xl mb-3 font-serif">Hotel Information</h2>
                         <p>
-                            We are excited to work with The Box Elder Events Center to host guests during the 2025 Black Hills Defense & Industry Symposium.
+                            We are excited to work with The Box Elder Events Center to host guests during the 2026 Defense & Industry Symposium.
                         </p>
                         <p>
-                            To book, use the link below or
-                            contact the Courtyard at <Link className="text-nowrap" href="tel:605-791-0945">(605) 791-0945</Link>, mention “2025 Black Hills Defense & Industry Symposium” when booking.
+                            To book, use the link below or contact the Courtyard at <Link className="text-nowrap" href="tel:605-791-0945">(605) 791-0945</Link>, mention “2026 Defense & Industry Symposium” when booking.
+                        </p>
+                        <p>
+                            You will find the information for your online reservation link below.
                         </p>
                         <p>
                             <Link
-                                href="https://www.marriott.com/event-reservations/reservation-link.mi?id=1719931537591&key=GRP&guestreslink2=true&app=resvlink"
-                                className="flex justify-center my-4
-                        ">
-                                <span
-                                    className="bg-primary-600 hover:bg-primary-500 transition-colors text-primary-100 py-2 px-16 rounded-lg">Book Now</span>
+                                href="https://www.marriott.com/event-reservations/reservation-link.mi?id=1757602875230&key=CORP&app=resvlink"
+                                className="underline text-primary-700 hover:text-primary-600"
+                            >
+                                Book your corporate rate for Black Hills Military Advisory Coalition
                             </Link>
+                        </p>
+                        <p>
+                            Participating properties:
+                            <br />
+                            Courtyard by Marriott Rapid City
                         </p>
                         <p>
                             Parking is free and conveniently located near The Box Elder Events Center.
